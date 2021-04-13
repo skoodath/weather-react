5fd86e1a5dad5946572fa06ac6cabdc664114aa7
Shiju Nambiar <>
2021-04-13 17:03:15 +0530

    changed favicon and added react helment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Wrapper from './components/wrapper';
 import GlobalStyle from './styles/globalstyles';
+import { Helmet } from 'react-helmet';
 
 //import Error from './components/error';
 
@@ -10,6 +11,15 @@ const App = () => {
 
   return (
       <>
+        <Helmet>
+          <title>Daily Weather Snapshot</title>
+          <meta name="description" content="An web app to lookup current weather and five days weather forecast" />
+          <meta property="og:title" content="Daily Weather Snapshot" />
+          <meta property="og:description" content="An web app to lookup current weather and five days weather forecast" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://weathersnap.netlify.app/" />
+          <meta property="twitter:site" content="https://weathersnap.netlify.app/" />
+        </Helmet>
         <Wrapper />
         <GlobalStyle />
       </>
